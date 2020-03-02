5853246852b2e49d0e5541952ccce012b0580c1b
Bening Promistya <bening.promistya.tif18@polban.ac.id>
2020-03-02 15:11:23 +0700

    RV

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.recycleview;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Herdi_WORK on 15.09.16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
+
+    }
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("PPB - Fan");
+        dataSet.add("Proyek - Survey");
+        dataSet.add("StatPro - Peluang");
+        dataSet.add("PPB - Install Android Studio");
+    }
+}
